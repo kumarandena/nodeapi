12248479989853d29b3cc12da29c247fbe55b3a5
kumarandena <kumarandena@gmail.com>
2019-07-01 01:19:46 +0530

    kick-off

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+// bring in routes
+const { getPosts } = require('./routes/post')
+
+app.get("/", getPosts);
+ 
+const port = 8080;
+app.listen(port, () => {
+    console.log(`A Node Js API is listening on port: ${port}`)
+});
